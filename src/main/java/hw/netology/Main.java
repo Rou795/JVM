58f5bd11afd50eeadf65455a33c7498ebcba4ba6
Rou795 <jon.79555@gmail.com>
2024-01-24 10:28:13 +0700

    задания завершены, решение в task_1,2.md

---
+++ src/main/java/hw/netology/Main.java
@@ -0,0 +1,23 @@
+package hw.netology;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+
+    public class JvmComprehension {
+
+        public static void main(String[] args) {
+            int i = 1;                      // 1
+            Object o = new Object();        // 2
+            Integer ii = 2;                 // 3
+            printAll(o, i, ii);             // 4
+            System.out.println("finished"); // 7
+        }
+
+        private static void printAll(Object o, int i, Integer ii) {
+            Integer uselessVar = 700;                   // 5
+            System.out.println(o.toString() + i + ii);  // 6
+        }
+    }
+}
